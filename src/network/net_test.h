f34442bc5fb46dfb2bd1e642bf870bd40fb48b98
zjm <zheng39562@163.com>
2016-10-11 23:21:23 +0800

    增加对应的nettest设计。未实现。

--- src/network/net_test.h
+++ src/network/net_test.h
@@ -14,7 +14,36 @@
 //! \note	用于network的测试。
 //! \note	单元测试，回归测试都可用。
 //! \note	该代码可读性不佳，仅用于测试，可作为了解参考。
+//! \note	测试方法。将该函数在main函数中调用。
+//!				服务端输入参数server，客户端输入参数client.
+//!				测试该功能需要启动2个进程。
 
+//! \note	该命名空间仅用于测试network。任何其他功能都不应该放在该空间内。
+namespace NetworkTest{
+	//! \brief	总测试接口
+	void doTest(const string &arg);
+	//! \brief	连通性测试。
+	void doLink(const string &arg);
+
+	struct DataStruct{
+		int i;
+		double d;
+		char c;
+	};
+	class DataClass{
+		int i;
+		double d;
+		char c;
+	};
+	//! \brief	不包含字符串的结构体和类是否能够直接通过memcpy进行赋值的测试。
+	void doMemCpy(const string &arg);
+
+	const int PERFROMANCE_SIZE;
+	//! \brief	数据包准确性测试，以及10W个40-50B的包的性能测试。
+	//! \note	数据统计存储在客户端和服务端。
+	//! \note	性能测试如果需要考虑tcp传输时间，则需要有对应的2台机器。
+	void doPerformance(const string &arg);
+}
 
 
 
