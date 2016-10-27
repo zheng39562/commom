9b8179437af0c031db87ae1619654a98c46b2fc2
zheng39562 <zheng39562@163.com>
2016-10-27 08:00:16 -0400

    增加Byte类型。

--- src/fr_public/pub_define.h
+++ src/fr_public/pub_define.h
@@ -1,12 +1,9 @@
 /**********************************************************
- *  \!file pub_define.h
- *  \!brief
- *  \!note	注意事项： 
- * 			1,类中的成员函数中的同名参数的含义完全相同。仅会注释其中一个函数，其他函数则不再重复注释。重名的参数意义不同时，会独立注解。 
- * 			2,第1条的规则同样适用于返回值的含义。 
+ *  \file pub_define.h
+ *  \note	公共的define。基本所有项目公用。
  * 
- * \!version 
- * * \!author zheng39562@163.com
+ * \version 
+ * * \author zheng39562@163.com
 **********************************************************/
 #ifndef _pub_define_H
 #define _pub_define_H
@@ -23,6 +20,7 @@
 using namespace std;
 typedef string Name;
 typedef string Path;
+typedef unsigned char Byte;
 
 /// *******************************************************************************************************
 
